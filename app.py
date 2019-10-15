e3a879aa5bd273b12e8a14d579ebd12c06956357
Calugar-Pop Raul <calugar-popraul@Calugar-Pops-MacBook.local>
2019-10-15 12:15:48 +0300

    update

--- app.py
+++ app.py
@@ -21,17 +21,6 @@ class NeuralNetwork:
         self.spinboxInputs = Spinbox(self.f, from_=0, to=10, width=5)
         self.spinboxInputs.grid(row=1, column=0)
 
-        # self.labelHl = Label(self.f, text="Hidden Layers")
-        # self.labelHl.grid(row=0, column=1)
-        # self.spinboxHl = Spinbox(self.f, from_=0, to=10, width=5)
-        # self.spinboxHl.grid(row=1, column=1)
-
-        # self.labelNeurons = Label(self.f, text="Neurons")
-        # self.labelNeurons.grid(row=0, column=2)
-        # self.spinboxNeurons = Spinbox(self.f, from_=0, to=10, width=5)
-        # self.spinboxNeurons.grid(row=1, column=2)
-
-
         self.createSpinboxList = Button(
             self.f, text="Generate Neural Network", 
             command=self.generateInputs
@@ -70,8 +59,6 @@ class NeuralNetwork:
         self.neurons = [0.000] * nrOfInputs
 
         print(self.neurons)
-        # nrOfHl = int(self.spinboxHl.get())
-        # nrOfNeurons = int(self.spinboxNeurons.get())
 
         for i in range(nrOfInputs):
             curSb = 'inputSpinbox' + str(i) 
@@ -113,34 +100,7 @@ class NeuralNetwork:
             command=self.sum
             )            
         self.createCalculateInputsButton.grid(row=2, column=2)
-
-
-        # self.neurons = np.zeros(nrOfNeurons)
-        # for i in range(nrOfNeurons):
-            # curWb = 'weightSpinbox'+ str(i)
-            # labelWb = 'labelNeuron' + str(i)
-            # curWb = Spinbox(
-            #     from_=0.001, 
-            #     to=10.000, 
-            #     increment=0.001,
-            #     width=10,
-            # )
-            # labelWb = Label(
-            #     self.f,
-            #     text="neuron"
-            # )
-            # curWb.place(x=xWs, y=yWs)
-            # labelWb.place(x=180, y=yWs)
-
-            # yWs+=40
-            # self.weights.append(curWb)
-            # self.labelNeurons.append(labelWb)
-
-
-
-
         
-
 
 root = Tk()
 root.geometry('500x500')
